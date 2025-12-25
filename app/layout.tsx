3e945319a7a95a64eb58a95532d5c6d06a8a69a8
Anthony Okpako <melowsworld@yahoo.co.uk>
2025-12-25 02:06:21 +0100

    Added SEO stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL!;
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
